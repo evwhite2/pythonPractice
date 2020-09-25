9dff07a70f95497b76ea6d0e16dabd730d5bb865
Ellen <ellen.white@fdmgroup.com>
2020-09-25 17:16:25 -0500

    still working on unique word identification

--- fdm-ex-3/emailstats.py
+++ fdm-ex-3/emailstats.py
@@ -10,35 +10,50 @@ def writeStats(total, wordCount, avgWordCount, standardDeviation, totalBytes):
     statFile.write(stats)
     statFile.close()
 
+def writeTable(unique_words):
+    result=unique_words
+    print(result)
+
 def openAll():
     total=0
     wordCount=0
     avgWordCount=0
     standardDeviation=0.0
     totalBytes=0
-    files_data=[]
+    agg_wordCount_data=[]
+    unique_wordCount_data=[]
     for file in os.listdir(__dirName):
         total+=1
         with open(os.path.join(__dirName, file), encoding="utf8") as f:
             wordCount=0
+            unique_words=[]
             try:
                 for line in f:
                     line_data=line.split(" ")
                     counter = wordCounter(line_data)
-                    wordCount+=counter
+                    wordCount+=counter[0]
                     bCounter = byteCounter(line)
                     totalBytes+=bCounter
+                    for word in counter[1]:
+                        uniqueness = isUnique(unique_wordCount_data, word)
+                        if isUnique:
+                            unique_wordCount_data.append(word)
+                        else:
+                            continue
             except:
                 error_file_name= f.name
                 error_local=f.tell
                 print("ERROR at file: "+error_file_name+"___at position: "+error_local)
             finally:
-                files_data.append(wordCount)
-    print("\nWriting to email_info.txt...")
-    standardDeviation=round(statistics.stdev(files_data), 2)
-    wordCount=(sum(files_data, 0))
+                agg_wordCount_data.append(wordCount)
+    print("\nWriting stats to files...")
+    standardDeviation=round(statistics.stdev(agg_wordCount_data), 2)
+    wordCount=(sum(agg_wordCount_data, 0))
     avgWordCount=round(wordCount/total, 2)
-    writeStats(total, wordCount, avgWordCount, standardDeviation, totalBytes)
+    # writeStats(total, wordCount, avgWordCount, standardDeviation, totalBytes)
+    unique_wordCount_data.sort()
+    unique_wordCount_data=set(unique_wordCount_data)
+    writeTable(unique_wordCount_data)
 
 def byteCounter(line):
     count=0
@@ -48,12 +63,26 @@ def byteCounter(line):
 
 def wordCounter(line):
     count=0
+    unique_words=[] #NOT SURE WHAT TO DO HERE, WILL REVISIT
     for word in line:
-        if word=="\n":
+        sub = list(word)
+        if sub[-1]=="\n":
+            word = "".join(sub[-1:])
             continue
         else:
             count+=1
-    return count
+            unique= isUnique(unique_words, word)
+            if unique:
+                unique_words.append(word)
+            else:
+                continue
+    return [count, unique_words]
+
+def isUnique(array, word):
+    if word in array:
+        return 0
+    else:
+        return 1
 
 openAll()
 
