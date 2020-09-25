e82e3dc5d53c7adb762bc4f3af19f760db32a6e2
Ellen <ellen.white@fdmgroup.com>
2020-09-25 13:50:28 -0500

    all stats available

--- fdm-ex-3/emailstats.py
+++ fdm-ex-3/emailstats.py
@@ -1,3 +1,4 @@
+import statistics
 import os
 from os import path
 __dirName='C:/Users/evwhi/PythonCamp/fdm-ex-3/emails/'
@@ -9,25 +10,38 @@ def getStats(total, wordCount, avgWordCount, standardDeviation, totalBytes):
 def openAll():
     total=0
     wordCount=0
-    avgWordCount=0.0
+    avgWordCount=0
     standardDeviation=0.0
     totalBytes=0
     files_data=[]
     for file in os.listdir(__dirName):
         total+=1
-        with open(os.path.join(__dirName, file, ), encoding="utf8") as f:
+        with open(os.path.join(__dirName, file), encoding="utf8") as f:
+            wordCount=0
             try:
                 for line in f:
                     line_data=line.split(" ")
                     counter = wordCounter(line_data)
                     wordCount+=counter
+                    bCounter = byteCounter(line)
+                    totalBytes+=bCounter
             except:
                 error_file_name= f.name
-                print("ERROR at file: "+error_file_name)
+                error_local=f.tell
+                print("ERROR at file: "+error_file_name+"___at position: "+error_local)
             finally:
-                continue
+                files_data.append(wordCount)
     print("\n.....getting stats")
+    standardDeviation=round(statistics.stdev(files_data), 2)
+    wordCount=(sum(files_data, 0))
+    avgWordCount=round(wordCount/total, 2)
     getStats(total, wordCount, avgWordCount, standardDeviation, totalBytes)
+
+def byteCounter(line):
+    count=0
+    for b in line:
+        count+=1
+    return count
 
 def wordCounter(line):
     count=0
