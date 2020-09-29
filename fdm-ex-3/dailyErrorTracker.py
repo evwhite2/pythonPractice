9a1965b159a0b66225c3f2ba362c86efad9ea456
Ellen <ellen.white@fdmgroup.com>
2020-09-29 17:37:55 -0500

    findNegative (Errors) function added

--- fdm-ex-3/dailyErrorTracker.py
+++ fdm-ex-3/dailyErrorTracker.py
@@ -1,15 +1,23 @@
+import re
 __dirName = 'C:/Users/evwhi/PythonCamp/fdm-ex-3/'
 
-found_errors= []
 
 def findNegatives(stream_data, main_data):
+    found_errors=""
+    found_errors_array= []
     while len(stream_data) > 0:
         batch = stream_data[:10]
-        # print("BATCH")
+        for record in batch:
+            for data in record:
+                if data==str(-1):
+                    found_errors=found_errors+"\n"+str(record)
+                    found_errors_array.append(record)
+                else: continue
         stream_data= stream_data[10:]
+    addToCsv(found_errors, found_errors_array)
 
 def findErrors(stream_data_file, main_data_file):
-    found_errors=dict()
+    # found_errors=dict()
     stream_data= []
     main_data= []
     stream_data_file = open(__dirName+stream_data_file, 'r', encoding='utf8')
@@ -22,10 +30,20 @@ def findErrors(stream_data_file, main_data_file):
         main_data.append(line)
     stream_data_file.close()
     main_data_file.close()
+    field_names="index"
+    for field in main_data[0]:
+        field_names= field_names+","+field 
+    field_names=field_names+"\n"
+    file = open(__dirName+'daily_errors.csv', 'w', encoding='utf8')
+    file.write(field_names)
+    file.close()
     findNegatives(stream_data, main_data)
 
 def streamLines(line):
     list_stream = line.split(",")
+    last = list_stream[-1]
+    last = re.sub(r"\n$", "", last, flags=re.I)
+    list_stream[-1] = last
     return list_stream
 
 def sumErrors(list):
@@ -35,5 +53,31 @@ def sumErrors(list):
     else:
         return 0
 
+def addToCsv(data_string, data_array):
+    file = open(__dirName+'daily_errors.csv', 'a', encoding='utf8') 
+    count=1
+    for record in data_array:
+        stringify = str(count)
+        for data in record:
+            stringify=stringify+","+data    
+        stringify=stringify+'\n'
+        file.write(stringify)
+        count+=1
+    
 
 findErrors('daily_trades.csv', 'daily_trades_master.csv')
+
+# field_string = "index"
+#     for field in fields:
+#         field_string=field_string+","+field
+#     field_string=field_string+"\n"
+#     csvFile = open(__dirName+csvFile_string, 'w+', encoding='utf-8') 
+#     csvFile.write(field_string)
+#     record_string=""
+#     for key, value in records.items():
+#         view = str(key)
+#         for i in value:
+#             view = view+","+i
+#         record_string=record_string+view+"\n"
+#         csvFile.writelines(view+"\n")
+#     csvFile.close()
